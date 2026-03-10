975acfb3ece4ca64fe744c8d7fdda3e6209ae3f2
KeihanikakauLOL <arm.nantana@gmail.com>
2026-03-10 21:59:32 +0700

    done

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
 //Write function count() here.
+int count(int arr[], int size) {
+	set<int> temp;
+	for (int i = 0; i < size; i++) {
+		temp.insert(arr[i]);
+	}
+
+	return temp.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
